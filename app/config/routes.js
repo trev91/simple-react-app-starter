ee9e3f60712ae410131def71ff36793ca7956c20
Trevor Hardy <trevorhardy91@gmail.com>
2017-01-09 15:47:26 -0700

    added routes config

---
+++ app/config/routes.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var hashHistory = ReactRouter.hashHistory;
+var IndexRoute = ReactRouter.IndexRoute;
+
+var routes = (
+  <Router history={hashHistory}>
+
+  </Router>
+);
+
+module.exports = routes;
